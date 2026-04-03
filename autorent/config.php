8439fe291af5fed8e4518b6e91c128be03c34844
Marten-Jyrgen Kuusk <mkuusk@hkhk.edu.ee>
2026-04-03 12:01:08 +0000

    klgdnsglksdnglksngskl

--- autorent/config.php
+++ autorent/config.php
@@ -1,14 +1,11 @@
 <?php
-    // Sinu andmed
     $db_server = 'localhost';
-    $db_andmebaas = 'autorent';
+    $db_andmebaas = 'car_rent';
     $db_kasutaja = 'mario';
-    $db_salasona = 'mario';
+    $db_salasona = 'Mario#2026!';
 
-    // Ühendus andmebaasiga
     $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 
-    // Ühenduse kontroll
     if (!$yhendus) {
         die('Ei saa ühendust andmebaasiga');
     }
